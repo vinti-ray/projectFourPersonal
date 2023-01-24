4787ae0945fccc9a410e7659954350411599b065
vinti-roy <vintiray71@gmail.com>
2023-01-25 00:19:56 +0530

    done

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,39 +1,33 @@
 const userModel = require("../models/userModel")
-const { isValidEmail, isValidMobile, isValidName, isValidpassword } = require("../validation/validation");
-const check = require("check-types")
+const {userJOI,loginJoi}=require("../validation/joiValidation")
 const jwt = require("jsonwebtoken")
 
 const createUser = async function (req, res) {
     try {
-        let data = req.body
-        let { title, name, phone, email, password } = data
-        // if (typeof phone != "string" ) return res.status(400).send({ status: false, message: "type should be string" })
-        if (!name) return res.status(400).send({ status: false, message: "name is required" })
-        if (!isValidName(name)) return res.status(400).send({ status: false, message: "name is not valid" })
 
-        if (!title) return res.status(400).send({ status: false, message: "title is required" })
-        if (title != ("Mr" || "Mrs" || "Miss")) return res.status(400).send({ status: false, message: "title can only be mr,mrs.miss" })
+        let data=req.body
 
-        if (!phone) return res.status(400).send({ status: false, message: "phone no is required" })
-        if (!isValidMobile(phone)) return res.status(400).send({ status: false, message: "phone no is not valid" })
-        if (!check.string(phone)) return res.status(400).send({ status: false, message: "phone type should be string" })
-        if (!email) return res.status(400).send({ status: false, message: "email is required" })
-        if (!isValidEmail(email)) return res.status(400).send({ status: false, message: "email is not valid" })
+        if (Object.keys(data).length == 0)return res.status(400).send({ status: false, msg: "please send some data" });
 
-        if (!password) return res.status(400).send({ status: false, message: "password is required" })
-        if (!isValidpassword(password)) return res.status(400).send({ status: false, message: "password is not valid" })
+	    let one=0
+	    const validation = await userJOI.validateAsync(data).then(()=>true).catch((err)=>{one=err.message; return null})
+	
+	    if(!validation) return res.status(400).send({status :false,message:`${one}`})
 
-        let isExisting = await userModel.findOne({ $or: [{ email: email }, { phone: phone }] })           //checkk
-        if (isExisting) {
-            if (isExisting.email == email.trim()) {
-                return res.status(400).send({ status: false, message: "email is already there" })
-            } else {
-                (isExisting.phone == phone.trim())
-                return res.status(400).send({ status: false, message: "phone is already there" })
-            }
-        }
-        let create = await userModel.create(data)
-        return res.status(201).send({ status: true, message: "Success", data: create })
+
+	    let isExisting = await userModel.findOne({ $or: [{ email: data.email.trim() }, { phone: data.phone.trim() }] })           
+	    if (isExisting) {
+	        if (isExisting.email == data.email.trim()) {
+	            return res.status(400).send({ status: false, message: "email is already there" })
+	        } else {
+	            (isExisting.phone == data.phone.trim())
+	            return res.status(400).send({ status: false, message: "phone is already there" })
+	        }
+	    }
+        
+	    let create = await userModel.create(data)
+	    return res.status(201).send({ status: true, message: "Success", data: create })
+
     } catch (error) {
         return res.status(500).send({ error: error.message })
     }
@@ -43,18 +37,27 @@ const createUser = async function (req, res) {
 
 const loginUser = async function (req, res) {
    try {
-	 let email = req.body.email 
-	    let password = req.body.password
-	    if (!email) return res.status(404).send({ status: false, message: "email is missing" })
-	    if (!password) return res.status(404).send({ status: false, message: "password is missing" })
-	    let getUser = await userModel.findOne({ email: email, password: password })
-	    if (!getUser) {
-	        return res.status(404).send({ status: false, message: "no user with this id and password" })
-	    } else {
-	        let token = jwt.sign({ userId: getUser._id },"groupseven",{expiresIn:"3000000"})
-            res.setHeader("x-auth-key",token)
-	        return res.status(200).send({ status: true, message:"Success" ,data:token})
-        }
+    let data=req.body
+
+    if (Object.keys(data).length == 0)return res.status(400).send({ status: false, msg: "please send some data" });
+    
+       let one=0
+       const validation = await loginJoi.validateAsync(data).then(()=>true).catch((err)=>{one=err.message; return null})
+   
+       if(!validation) return res.status(400).send({status :false,message:`${one}`})
+
+       let email = data.email.trim()
+       let password = data.password.trim()
+
+       let getUser = await userModel.findOne({ email: email, password: password })
+       if (!getUser) {
+           return res.status(404).send({ status: false, message: "no user found with this credential" })
+       } else {
+           let token = jwt.sign({ userId: getUser._id },"groupseven",{expiresIn:"30000"})
+           res.setHeader("x-auth-key",token)
+           return res.status(200).send({ status: true, message:"Success" ,data:token})
+       }
+
 	
 } catch (error) {
 	return res.status(500).send({ error: error.message })
--- src/validation/joiValidation.js
+++ src/validation/joiValidation.js
@@ -1,12 +1,12 @@
 const Joi = require('joi');
 
-const schema = Joi.object({
+const bookJoi = Joi.object({
     
       title: Joi.string().required(),
 
       excerpt: Joi.string().required(),
 
-      ISBN: Joi.string().required().regex(/^(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-]+$/),
+      ISBN: Joi.string().required().regex(/^(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-]+$/).message("please enter valid ISBN"),
 
       userId:Joi.required(),
 
@@ -24,20 +24,20 @@ const schema = Joi.object({
 const userJOI=Joi.object({
   title: Joi.string().required().valid("Mr", "Mrs", "Miss"),
 
-  name: Joi.string().required().regex(/^[a-zA-Z ]+$/),
+  name: Joi.string().required().regex(/^[a-zA-Z ]+$/).message("please enter valid name"),
  
-  phone: Joi.string().required().regex(/^[0]?[6789]\d{9}$/),
+  phone: Joi.string().trim().required().regex(/^[0]?[6789]\d{9}$/).message("phone is not valid"),
 
-  email:Joi.string().required().regex(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/),
+  email:Joi.string().trim().required().regex(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/).message("please enter valid email"),
 
-  password: Joi.string().required().min(8).max(15).regex(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,15}$/),
+  password: Joi.string().trim().required().min(8).max(15).regex(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,15}$/).message("password  should contain Min 8 character and 1 Special Symbol"),
 
   address: Joi.object({
     street: Joi.string(),
 
     city: Joi.string(),
 
-    pincode:Joi.string().regex(/^([0-9]{4}|[0-9]{6})$/)
+    pincode:Joi.string().regex(/^([0-9]{4}|[0-9]{6})$/).message("please enter valid pin")
   })
   
 
@@ -62,11 +62,11 @@ const userJOI=Joi.object({
     })
 
     const loginJoi=Joi.object({
-      email:Joi.string().required().regex(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/),
-      password: Joi.string().required().min(8).max(15).regex(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,15}$/),
+      email:Joi.string().trim().required().regex(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/).message("please enter valid email"),
+      password: Joi.string().trim().required().min(8).max(15).regex(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,15}$/).message("please enter valid password"),
     })
 
-//const {title,excerpt,releasedAt,ISBN}=data
+
     const updateJoi=Joi.object({
       title: Joi.string(),
 
@@ -80,4 +80,8 @@ const userJOI=Joi.object({
 
 
 
-    module.exports={schema,reviewJoi,userJOI,loginJoi,updateJoi}
+    module.exports={bookJoi,reviewJoi,userJOI,loginJoi,updateJoi}
+
+
+
+    //add trim and message in isbn part
